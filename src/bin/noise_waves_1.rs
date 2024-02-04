5325e188b8a6ece60a2bb4794c086114e8809d7b
Alberto Re <alberto.re@gmail.com>
2024-02-04 15:17:48 +0100

    Working on noise waves

--- src/bin/noise_waves_1.rs
+++ src/bin/noise_waves_1.rs
@@ -1,31 +1,57 @@
 use anyhow::Result;
 use geo::coord;
-use noise::Cylinders;
 use noise::NoiseFn;
+use noise::Perlin;
 use plt::layout::Orientation::Landscape;
 use plt::layout::PageLayout;
 use plt::render::render_svg;
-use plt::shapes::Circle;
+use plt::shapes::Rect;
 use plt::Group;
 use plt::Sketch;
 use plt::Style;
+use rand::rngs::StdRng;
+use rand::Rng;
+use rand::SeedableRng;
 
 fn main() -> Result<()> {
     let mut sketch = Sketch::new(PageLayout::axidraw_minikit(Landscape));
-    let mut group = Group::new();
-    let cyl = Cylinders::new();
+    let perlin = Perlin::new(5);
+    let mut rng = StdRng::seed_from_u64(42);
 
-    (3..70).for_each(|i| {
-        (4..70).for_each(|j| {
-            let x = (8 * i) as f64;
-            let y = (5 * j) as f64;
-            let val = cyl.get([x * 0.02, y * 0.02]);
-            let circle = Circle::new(coord! {x: x, y: y}, val * 20.);
-            group.add_circle(&circle);
-        })
+    let dx: isize = 27;
+    let dy: isize = 18;
+    let spc: isize = 10;
+    let minrad: usize = 1;
+    let maxrad: usize = 8;
+    let noiseratio: f64 = 0.008;
+
+    let mut group1 = Group::new();
+    let mut group2 = Group::new();
+    (-dx..dx).for_each(|i| {
+        (-dy..dy).for_each(|j| {
+            let mut x = (spc * i) as f64;
+            let mut y = (spc * j) as f64;
+            x += sketch.centroid().x;
+            y += sketch.centroid().y;
+            let val = perlin.get([x * noiseratio, y * noiseratio]);
+            let val = (val + 1.0) / 2.;
+            let val = minrad + 1 + (val * (maxrad - minrad) as f64) as usize;
+            if (rng.gen::<f64>() + 0.1) * sketch.as_rect().width < x {
+                (minrad..val).for_each(|v| {
+                    let rect = Rect::square_with_center(coord! {x: x, y: y}, v as f64);
+                    group1.add_rect(&rect);
+                })
+            } else {
+                (minrad..val).for_each(|v| {
+                    let rect = Rect::square_with_center(coord! {x: x, y: y}, v as f64);
+                    group2.add_rect(&rect);
+                })
+            }
+        });
     });
+    sketch.add_group(&group1, &Style::new("red", "1.5px"));
+    sketch.add_group(&group2, &Style::new("blue", "1.5px"));
 
-    sketch.add_group(&group, &Style::new("black", "1px"));
     render_svg(&sketch, "./samples/noise_waves_1.svg")?;
     Ok(())
 }
---
+++ src/bin/noise_waves_2.rs
@@ -0,0 +1,46 @@
+use anyhow::Result;
+use geo::coord;
+use noise::NoiseFn;
+use noise::Perlin;
+use plt::layout::Orientation::Landscape;
+use plt::layout::PageLayout;
+use plt::render::render_svg;
+use plt::shapes::LineString;
+use plt::Group;
+use plt::Sketch;
+use plt::Style;
+
+fn main() -> Result<()> {
+    let mut sketch = Sketch::new(PageLayout::axidraw_minikit(Landscape));
+    let perlin = Perlin::new(29);
+
+    let dx: isize = 35;
+    let dy: isize = 40;
+    let spcy: isize = 4;
+    let spcx: isize = 8;
+    let minrad: usize = 0;
+    let maxrad: usize = 5;
+    let noiseratio: f64 = 0.005;
+
+    let mut group = Group::new();
+    (-dy..dy).for_each(|j| {
+        let mut points = vec![];
+        (-dx..dx).for_each(|i| {
+            let mut x = (spcx * i) as f64;
+            let mut y = (spcy * j) as f64;
+            x += sketch.centroid().x;
+            y += sketch.centroid().y;
+            let val = perlin.get([x * noiseratio, y * noiseratio]);
+            let val = (val + 1.0) / 2.;
+            let val = minrad + 1 + (val * (maxrad - minrad) as f64) as usize;
+            let val = val * 12;
+            points.push(coord! { x: x, y: y + val as f64 });
+        });
+        let lstr = LineString::new(points);
+        group.add_lstr(&lstr);
+    });
+    sketch.add_group(&group, &Style::new("black", "2.5px"));
+
+    render_svg(&sketch, "./samples/noise_waves_2.svg")?;
+    Ok(())
+}
