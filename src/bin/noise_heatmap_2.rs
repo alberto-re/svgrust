d727ad781e0da7788150e6f9a926d9e1ab5c0ae7
Alberto Re <alberto.re@gmail.com>
2024-02-24 13:53:46 +0100

    Add noise heatmap 2

--- src/bin/noise_heatmap_2.rs
+++ src/bin/noise_heatmap_2.rs
@@ -12,13 +12,14 @@ use plt::Style;
 
 fn main() -> Result<()> {
     let mut sketch = Sketch::new(&PageLayout::axidraw_minikit(Portrait));
-    let mut group = Group::new();
+    let mut group1 = Group::new();
+    let mut group2 = Group::new();
 
     let perlin = Perlin::new(2);
-    let square_side = 5.;
+    let square_side = 8.;
     let noise_ratio = 0.005;
     let radius_min = 0.0;
-    let radius_interval = 6.;
+    let radius_interval = 7.;
     let grid_box = sketch.as_rect();
     let grid_box = grid_box.scale(0.98);
 
@@ -27,14 +28,23 @@ fn main() -> Result<()> {
     grid.iter_centers().for_each(|center| {
         let noise_val = perlin.get([center.x * noise_ratio, center.y * noise_ratio]);
         let noise_val = (noise_val + 1.) / 2.;
-        group.add_circle(&Circle::new(
+        group1.add_circle(&Circle::new(
             *center,
             radius_min + radius_interval * noise_val,
         ));
+        let mut inverse_noise_val = 1. - (noise_val + 1.) / 2.;
+        if f64::abs(noise_val - inverse_noise_val) < 0.1 {
+            inverse_noise_val -= 0.2;
+        }
+        group2.add_circle(&Circle::new(
+            *center,
+            radius_min + radius_interval * inverse_noise_val,
+        ));
     });
 
-    sketch.add_group(&group, &Style::new("black", "1.0px"));
+    sketch.add_group(&group1, &Style::new("blue", "1.0px"));
+    sketch.add_group(&group2, &Style::new("violet", "1.0px"));
 
-    render_svg(&sketch, "./samples/noise_heatmap.svg")?;
+    render_svg(&sketch, "./samples/noise_heatmap_2.svg")?;
     Ok(())
 }
