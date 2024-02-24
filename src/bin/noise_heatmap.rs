dfa3574948c31636b852c9c9c5a62d714bf1cf88
Alberto Re <alberto.re@gmail.com>
2024-02-24 13:35:36 +0100

    Add noise_heatmap

---
+++ src/bin/noise_heatmap.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use noise::NoiseFn;
+use noise::Perlin;
+use plt::layout::Orientation::Portrait;
+use plt::layout::PageLayout;
+use plt::render::render_svg;
+use plt::shapes::Circle;
+use plt::traits::Scale;
+use plt::Group;
+use plt::Sketch;
+use plt::Style;
+
+fn main() -> Result<()> {
+    let mut sketch = Sketch::new(&PageLayout::axidraw_minikit(Portrait));
+    let mut group = Group::new();
+
+    let perlin = Perlin::new(002);
+    let square_side = 5.;
+    let noise_ratio = 0.005;
+    let radius_min = 0.0;
+    let radius_interval = 6.;
+    let grid_box = sketch.as_rect();
+    let grid_box = grid_box.scale(0.98);
+
+    let grid = grid_box.into_square_grid(square_side);
+
+    grid.iter_centers().for_each(|center| {
+        let noise_val = perlin.get([center.x * noise_ratio, center.y * noise_ratio]);
+        let noise_val = (noise_val + 1.) / 2.;
+        group.add_circle(&Circle::new(*center, radius_min + radius_interval * noise_val));
+    });
+
+    sketch.add_group(&group, &Style::new("black", "1.0px"));
+
+    render_svg(&sketch, "./samples/noise_heatmap.svg")?;
+    Ok(())
+}
