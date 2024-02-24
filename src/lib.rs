a2929cfbab657e1e93a3580c784b676ec1400e14
Alberto Re <alberto.re@gmail.com>
2024-02-24 22:13:17 +0100

    Add map_range() utility

--- src/lib.rs
+++ src/lib.rs
@@ -121,3 +121,7 @@ impl Sketch {
         Rect::new(Vec2 { x: 0., y: 0. }, self.layout.width, self.layout.height)
     }
 }
+
+pub fn map_range(n: f64, start1: f64, stop1: f64, start2: f64, stop2: f64) -> f64 {
+    (n - start1) / (stop1 - start1) * (stop2 - start2) + start2
+}
