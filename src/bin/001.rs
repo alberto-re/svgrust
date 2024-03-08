60fe8d36dacaee4e83378bbb85c12e183e26c7c3
Alberto Re <alberto.re@gmail.com>
2024-03-08 10:42:25 +0100

    Remove sketch

--- src/bin/001.rs
+++ src/bin/001.rs
@@ -11,27 +11,41 @@ use plt::Group;
 use plt::Sketch;
 use plt::Style;
 
-fn sample_point(t: f64, l: f64, a: f64, b: f64, c: f64) -> Vec2 {
-    let x = (f64::cos(a * t) + f64::cos(b * t) / 2.0 + f64::sin(c * t) / 3.0) * l;
-    let y = (f64::sin(a * t) + f64::sin(b * t) / 2.0 + f64::cos(c * t) / 3.0) * l;
-    Vec2 { x, y }
+struct ParametricCurve {
+    l: f64,
+    a: f64,
+    b: f64,
+    c: f64,
 }
 
-fn get_curve_points(iterations: usize, step: f64, l: f64, a: f64, b: f64, c: f64) -> Vec<Vec2> {
-    let mut points: Vec<Vec2> = vec![];
-    let mut t = 0f64;
-    for _ in 0..iterations {
-        let next_point = sample_point(t as f64, l, a, b, c);
-        points.push(next_point);
-        t += step;
+impl ParametricCurve {
+    fn new(l: f64, a: f64, b: f64, c: f64) -> Self {
+        Self { l, a, b, c }
     }
-    points
+
+    fn points(&self, n: usize, interval: f64) -> Vec<Vec2> {
+        let mut points: Vec<Vec2> = vec![];
+        let mut t = 0f64;
+        for _ in 0..n {
+            let x =
+                (f64::cos(self.a * t) + f64::cos(self.b * t) / 2.0 + f64::sin(self.c * t) / 3.0)
+                    * self.l;
+            let y =
+                (f64::sin(self.a * t) + f64::sin(self.b * t) / 2.0 + f64::cos(self.c * t) / 3.0)
+                    * self.l;
+            points.push(Vec2 { x, y });
+            t += interval;
+        }
+        points
+    }
 }
 
 fn main() -> Result<()> {
     let mut sketch = Sketch::new(&PageLayout::axidraw_minikit(Portrait));
 
-    let points = get_curve_points(711, 1., 105., 1., 5., 7.)
+    let curve = ParametricCurve::new(105., 1., 5., 7.);
+
+    let points = curve.points(711, 1.)
         .iter()
         .map(|p| p.translate(sketch.as_rect().centroid()))
         .collect::<Vec<Vec2>>();
@@ -43,6 +57,7 @@ fn main() -> Result<()> {
     sketch.as_rect().grid(50, 30).iter().for_each(|rect| {
         plane.add_circle(&Circle::new(rect.centroid(), 0.1));
     });
+
     sketch.add_group(&plane, &Style::new("black", "0.2mm"));
     sketch.add_group(&curve, &Style::new("#093c80", "0.2mm"));
 
