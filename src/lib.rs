58167d6ce496af4d76ff4183a697e1fa42f5340b
Alberto Re <alberto.re@gmail.com>
2024-03-26 08:48:04 +0100

    Add clamp() function

--- src/lib.rs
+++ src/lib.rs
@@ -132,3 +132,7 @@ impl Default for Group {
 pub fn map_range(n: f64, start1: f64, stop1: f64, start2: f64, stop2: f64) -> f64 {
     (n - start1) / (stop1 - start1) * (stop2 - start2) + start2
 }
+
+pub fn clamp(n: f64, min: f64, max: f64) -> f64 {
+    f64::max(min, f64::min(max, n))
+}
