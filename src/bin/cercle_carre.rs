c4c4adc3b934e3b14f64b53eb02d00ca391b1fde
Alberto Re <alberto.re@gmail.com>
2024-03-29 15:02:23 +0100

    More on cercle carre

--- src/bin/cercle_carre.rs
+++ src/bin/cercle_carre.rs
@@ -29,6 +29,8 @@ fn main() -> Result<()> {
     let mut group1 = Group::new();
     let mut group2 = Group::new();
     let mut group3 = Group::new();
+    let mut group4 = Group::new();
+    let mut group5 = Group::new();
     add_square_spiral_with_center(
         sketch.as_rect().centroid(),
         sketch.as_rect().scale_perc(0.80).min_len(),
@@ -40,19 +42,35 @@ fn main() -> Result<()> {
         sketch.as_rect().centroid(),
         sketch.as_rect().scale_perc(0.80).min_len(),
         60,
-        TAU * 0.25 + 0.1,
+        TAU * 0.25 + 0.05,
         &mut group2,
     );
     add_square_spiral_with_center(
         sketch.as_rect().centroid(),
         sketch.as_rect().scale_perc(0.80).min_len(),
         60,
-        TAU * 0.25 + 0.2,
+        TAU * 0.25 + 0.1,
         &mut group3,
     );
+    add_square_spiral_with_center(
+        sketch.as_rect().centroid(),
+        sketch.as_rect().scale_perc(0.80).min_len(),
+        60,
+        TAU * 0.25 + 0.15,
+        &mut group4,
+    );
+    add_square_spiral_with_center(
+        sketch.as_rect().centroid(),
+        sketch.as_rect().scale_perc(0.80).min_len(),
+        60,
+        TAU * 0.25 + 0.2,
+        &mut group5,
+    );
     sketch.add_group(&group1, &Style::new("orange", "1.0px"));
-    sketch.add_group(&group2, &Style::new("blue", "1.0px"));
-    sketch.add_group(&group3, &Style::new("black", "1.0px"));
+    sketch.add_group(&group2, &Style::new("azure", "1.0px"));
+    sketch.add_group(&group3, &Style::new("blue", "1.0px"));
+    sketch.add_group(&group4, &Style::new("violet", "1.0px"));
+    sketch.add_group(&group5, &Style::new("black", "1.0px"));
     sketch.render().save_default()?;
     Ok(())
 }
